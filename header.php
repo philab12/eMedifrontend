3dbd72e2c9e3bbc8a58895ea24ae0480915313a1
kafski7 <kafski7@gmail.com>
2022-03-26 18:10:24 +0000

    Updated with links

--- header.php
+++ header.php
@@ -22,7 +22,7 @@
         </div>
         <nav class="navbar navbar-expand-lg navigation" id="navbar">
             <div class="container">
-                <a class="navbar-brand" href="index.html">
+                <a class="navbar-brand" href="index.php">
                     <img src="images/eMedipay-logo-small.png" alt="" class="img-fluid">
                 </a>
 
@@ -33,41 +33,41 @@
                 <div class="collapse navbar-collapse" id="navbarmain">
                     <ul class="navbar-nav ml-auto">
                         <li class="nav-item active">
-                            <a class="nav-link" href="index.html">Home</a>
+                            <a class="nav-link" href="index.php">Home</a>
                         </li>
-                        <li class="nav-item"><a class="nav-link" href="about.html">About</a></li>
-                        <li class="nav-item"><a class="nav-link" href="blog-sidebar.html">Medical Guide</a></li>
+                        <li class="nav-item"><a class="nav-link" href="about.php">About</a></li>
+                        <li class="nav-item"><a class="nav-link" href="blog-sidebar.php">Medical Guide</a></li>
 
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" href="#" id="dropdown02" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Department <i class="icofont-thin-down"></i></a>
                             <ul class="dropdown-menu" aria-labelledby="dropdown02">
-                                <li><a class="dropdown-item" href="department.html">Medical</a></li>
-                                <li><a class="dropdown-item" href="department-single.html">Pharmacy</a></li>
-                                <li><a class="dropdown-item" href="department.html">Dental</a></li>
-                                <li><a class="dropdown-item" href="department.html">Vision</a></li>
-                                <li><a class="dropdown-item" href="department.html">Medical Labs</a></li>
+                                <li><a class="dropdown-item" href="department.php">Medical</a></li>
+                                <li><a class="dropdown-item" href="department-single.php">Pharmacy</a></li>
+                                <li><a class="dropdown-item" href="department.php">Dental</a></li>
+                                <li><a class="dropdown-item" href="department.php">Vision</a></li>
+                                <li><a class="dropdown-item" href="department.php">Medical Labs</a></li>
                             </ul>
                         </li>
-                        <li class="nav-item"><a class="nav-link" href="service.html">Outreach Program</a></li>
+                        <li class="nav-item"><a class="nav-link" href="service.php">Outreach Program</a></li>
                         <!--
                         <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="doctor.html" id="dropdown03" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Doctors <i class="icofont-thin-down"></i></a>
+                            <a class="nav-link dropdown-toggle" href="doctor.php" id="dropdown03" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Doctors <i class="icofont-thin-down"></i></a>
                             <ul class="dropdown-menu" aria-labelledby="dropdown03">
-                                <li><a class="dropdown-item" href="doctor.html">Doctors</a></li>
-                                <li><a class="dropdown-item" href="doctor-single.html">Doctor Single</a></li>
-                                <li><a class="dropdown-item" href="appoinment.html">Appoinment</a></li>
+                                <li><a class="dropdown-item" href="doctor.php">Doctors</a></li>
+                                <li><a class="dropdown-item" href="doctor-single.php">Doctor Single</a></li>
+                                <li><a class="dropdown-item" href="appoinment.php">Appoinment</a></li>
                             </ul>
                         </li>
 
                         <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="blog-sidebar.html" id="dropdown05" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Blog <i class="icofont-thin-down"></i></a>
+                            <a class="nav-link dropdown-toggle" href="blog-sidebar.php" id="dropdown05" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Blog <i class="icofont-thin-down"></i></a>
                             <ul class="dropdown-menu" aria-labelledby="dropdown05">
-                                <li><a class="dropdown-item" href="service.html">Blog with Sidebar</a></li>
+                                <li><a class="dropdown-item" href="service.php">Blog with Sidebar</a></li>
 
-                                <li><a class="dropdown-item" href="blog-single.html">Blog Single</a></li>
+                                <li><a class="dropdown-item" href="blog-single.php">Blog Single</a></li>
                             </ul>
                         </li>-->
-                        <li class="nav-item"><a class="nav-link" href="contact.html">Contact</a></li>
+                        <li class="nav-item"><a class="nav-link" href="contact.php">Contact</a></li>
                     </ul>
                 </div>
             </div>
